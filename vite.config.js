cd51aa3028bd890bbcde5af99e941d4d7a4130b0
Jake Howard <git@theorangeone.net>
2023-07-01 16:01:26 +0100

    Move site source into src/ dir
    Keep the root cleaner

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [],
+  root: "./src",
+  base: "./",
   build: {
+    outDir: "../dist",
     emptyOutDir: true,
     target: "es2015"
   }
